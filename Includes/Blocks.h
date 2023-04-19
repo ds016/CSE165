eb6eccda6f7c56172d58e9fc79227950288d5bba
AMouillesseaux <amoucollege@gmail.com>
2023-04-19 09:04:42 -0700

    .h Files so they're there

---
+++ Includes/Blocks.h
@@ -0,0 +1,22 @@
+#ifndef BLOCKS_H
+#define BLOCKS_H
+
+class genBlock{
+public:
+int points=0;
+
+};
+
+class yBlock:public genBlock{
+
+};
+
+class oBlock:public genBlock{
+
+};
+
+class rBlock:public genBlock{
+
+};
+
+#endif
