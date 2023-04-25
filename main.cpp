9d016e6867eb3d6a1a38379b8630447075ab028c
AMouillesseaux <amoucollege@gmail.com>
2023-04-24 20:08:09 -0700

    Moved Player info to Player.h
    This was on the behalf of Dominique, who had some issues with uploading it

--- main.cpp
+++ main.cpp
@@ -1,50 +1,72 @@
 #include <GLFW/glfw3.h>
+#include <Ball.h>
+//#include <Blocks.h>
+#include <Player.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+// const variable
+const float PI = 3.14159265358979323f;
 
 // game window dimension
-int widthwindow = 1000;
-int heightwindow = 1000;
+int widthwindow = 720;
+int heightwindow = 720;
+Player player;
 
-// paddle's dimension
-float widthpaddle = 0.2f;
-float heightpaddle = 0.02f;
+// radius of ball
+float radiusball = 0.02f;
 
-// paddle initial position spawn
-float xpaddle = 0.0f;
-float ypaddle = -0.95f;
+// ball initial position spawn
+float xball = 0.0f; 
+float yball = 0.0f;
 
+// function to draw the ball model
+void createBall() {
+    // Drawing process 
+    glBegin(GL_TRIANGLE_FAN);
 
-// function to create the paddle
-void createPaddle() {
-
-    // Drawing process   
-    glBegin(GL_QUADS);
-
-    // set the rectangle to the color red 
+    // Set the color of ball model to red 
     glColor3f(1.0f, 0.0f, 0.0f);
 
-    // ********** IMPORTANT, WHEN CREATING SHAPES, MUST DRAW CCW, BOT-LEFT -> BOT-RIGHT -> TOP-RIGHT -> TOP-LEFT; **************
+    // Set center of the ball 
+    glVertex2f(xball, yball);
 
-    // Example code to create a red paddle in the middle of the window screen 
-    glVertex2f(xpaddle - (widthpaddle / 2), ypaddle);      // Bottom Left vertice
-    glVertex2f(xpaddle + (widthpaddle / 2), ypaddle);      // Bottom Right vertice 
+    float angle; 
 
-    glVertex2f(xpaddle + (widthpaddle / 2), ypaddle + heightpaddle);   // Top Right Vertice
-    glVertex2f(xpaddle - (widthpaddle / 2), ypaddle + heightpaddle);   // Top Left Vertice
+    // Increasing the loop end condition will result in a better drawn circle, uses triangles to create a circle shape, more triangle = more accurate circle
+    for (int i = 0; i < 10000; i++) {
+        // calculates the angle from each of the triangles being drawn (2PI is full circle)
+        angle = i * 2.0f * PI / 100;
+        // using sin/cos functions to determine the position multiplied by radius for sizing and add to x position for each triangle to draw a part of ball
+        glVertex2f(xball + cos(angle) * radiusball, yball + sin(angle) * radiusball);
+    }
 
-    // End drawing operation
-    glEnd();       
+    glEnd();
+
 }
 
-
+// function to handle mouse input for the movement of player
 void mousefunc(GLFWwindow* window, double xcord, double ycord) {
 
     // Determining the mouse position & converting that into the relative position on the application window
     float xposition = (float)xcord / (float)widthwindow * 2.0f - 1.0f;
-    xpaddle = xposition;
+    
+    // The coordinate plane is using normal coordinate values... min = -1, max = 1 on the x and y axis
+    float leftx = 0.9;
+    float rightx = -0.9;
+
+    // Checking if player model ever hits bounds, if bound is hit, condition. If not, player matches cursor 
+    if (xposition > leftx) {
+        player.setxpaddle(leftx);
+    }
+    else if (xposition < rightx) {
+        player.setxpaddle(rightx);
+    }
+    else {
+        player.setxpaddle(xposition);
+    }
 
 }   
 
@@ -82,7 +104,10 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // create the player model in the window context
-        createPaddle();
+        player.createPaddle();
+
+        // create the ball model in the window context 
+        createBall();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
